e7ff2320d1b2216f45d423b9b730b3c01bae5e7d
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-08-04 11:13:16 -0500

    Initial package file.

---
+++ R/package.R
@@ -0,0 +1,17 @@
+#' Graphrunner is a directed graph task execution engine
+#'
+#' Most collections of tasks can be represented as cyclic or
+#' acyclic directed graphs. This package makes the management
+#' and execution of such graphs of tasks straightforward.
+#' 
+#' @docType package
+#' @name graphrunner
+#' @import crayon R6
+#' @author Robert Krzyzanowski <\url{http://syberia.io}>
+#' @seealso The core function in this package: \code{\link{stagerunner}}. It
+#'   defines the constructor creating graphrunner objects that allow you to
+#'   wrap complicated task execution structures in a single object. 
+#' @references Full documentation and demos: \url{http://robertzk.github.io/graphrunner/};
+#'   FAQ's: \url{http://robertzk.github.io/graphrunner/faq/}
+NULL
+
