f10f38cd695f391f0c06da573b072214dba4d73a
Robert Krzyzanowski <technoguyrob@gmail.com>
2018-08-04 11:34:38 -0500

    Initial traversal strategies.

---
+++ R/traversal.R
@@ -0,0 +1,28 @@
+#' An R6 representation of a graph traversal strategy.
+#'
+#' A graph traversal strategy specifies how to execute tasks on
+#' a given graph.
+#'
+#' @name graphTraversalStrategy
+#' @format NULL
+#' @docType class
+graphTraversalStrategy <- R6::R6Class('graphTraversalStrategy',
+  public = list(
+    .visited = logical(0),
+    .strategy = identity
+  )
+)
+
+#' A breadth-first traversal strategy.
+#'
+#' This graph traversal strategy 
+#'
+#' @name graphTraversalStrategy
+#' @format NULL
+#' @docType class
+graphBFSTraversalStrategy <- R6::R6Class('graphBFSTraversalStrategy',
+  inherit = graphTraversalStrategy,
+  public = list(
+  )
+)
+
